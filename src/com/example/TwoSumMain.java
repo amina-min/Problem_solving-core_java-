b7880f9f955d926c184a71430b91bc8faed52127
Amina Khatun <aminaemcbd@gmail.com>
2022-02-26 00:57:18 +0600

    sprial print

--- src/com/example/TwoSumMain.java
+++ src/com/example/TwoSumMain.java
@@ -6,7 +6,7 @@ public class TwoSumMain {
 
 	public static void main(String[] args) {
 		int[] arr = {15,10,4,7,23};
-		int target =14 ;
+		int target =11 ;
 		
 		int[] sumarr = sumArray(arr, target);
 		System.out.print(Arrays.toString(sumarr));
